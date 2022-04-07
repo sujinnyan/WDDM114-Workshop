4c2c64cadc9dcf484b1da3d824e50b6a3af8a4e1
sujinnyan <97710314+sujinnyan@users.noreply.github.com>
2022-04-07 14:52:29 -0400

    Update slide.js

--- js/slide.js
+++ js/slide.js
@@ -5,10 +5,10 @@ const next = document.getElementById("next-button");
 const back = document.getElementById("back-button");
 
 const imgArray = [
-    "./img/image-1.jpg",
-    "./img/image-2.jpg",
-    "./img/image-3.jpg",
-    "./img/image-4.jpg",
+    "./img/Image-1.jpg",
+    "./img/Image-2.jpg",
+    "./img/Image-3.jpg",
+    "./img/Image-4.jpg",
 ];
 
 // EVENT LISTENERS
